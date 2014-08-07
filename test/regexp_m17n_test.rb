57a5e06752fadfe4bc880f52aa210e9027c64089
Pooyan Khosravi <pekhee@gmail.com>
2014-08-07 23:07:29 +0430

    only use encodings which have a converter present
    this is required to _run_ the test.

--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -6,7 +6,16 @@ class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
     encodings_as_string = Encoding.list.map(&:to_s)
 
-    encodings_as_string.each do |enc|
+    encodable_list = encodings_as_string.select do |enc|
+      begin
+        '.'.encode(enc)
+        true
+      rescue Encoding::ConverterNotFoundError
+        false
+      end
+    end
+
+    encodable_list.each do |enc|
       assert(RegexpM17N.non_empty?('.'.encode(enc)))
     end
   end
