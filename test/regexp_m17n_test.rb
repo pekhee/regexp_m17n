9d0846fe14d2263c15f3f63089ab2ed171b392b2
Pooyan Khosravi <pekhee@gmail.com>
2014-08-07 23:05:54 +0430

    we can't pass Encoding instance to String#encode
    however it is possible to pass string representation of that instance.

--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -4,7 +4,9 @@ require_relative '../lib/regexp_m17n'
 
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
-    Encoding.list.each do |enc|
+    encodings_as_string = Encoding.list.map(&:to_s)
+
+    encodings_as_string.each do |enc|
       assert(RegexpM17N.non_empty?('.'.encode(enc)))
     end
   end
